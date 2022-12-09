7a840842a10d016a2e4dc187a2d0d5e24f5f1959
Yuko8005 <yukoko6789@gmail.com>
2022-12-09 03:14:03 +0200

    вв

---
+++ main_3.py
@@ -0,0 +1,19 @@
+# завдання 2// номер 6
+a=0
+SSs=0
+
+def num():
+    global a, SSs
+    try:
+        a = int(input('ffffffffff '))
+        SSs = (a*(a+1))/2 if a>1 else 'введіть число більше 1!!!!!!!!!!!!!!!' 
+        print(SSs)
+        num()
+        
+        
+        
+    except:
+        print('введіть число!!!!!!!!')
+        num()
+
+num()

4a3c18fd273d44975d04064d64aaca37d8c1ef50
Yuko8005 <yukoko6789@gmail.com>
2022-12-09 03:30:06 +0200

    commit 2

---
+++ main_5.py
@@ -0,0 +1,11 @@
+import platform
+
+plat_str = platform.platform()
+processor = platform.processor()
+plat_str_0 = platform.python_implementation()
+plat_str_1 = platform.python_version()
+
+print("platform() = %s" % (plat_str))
+print('python_implementation() = %s' % (plat_str_0))
+print('python_version() = %s' % (plat_str_1))
+print('processor = %s' % (processor))
